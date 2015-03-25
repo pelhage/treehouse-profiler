65ad6afddbfda87fae8eadb34eef6eb01b5cf87b
Patrick E <Patrick@Patricks-MacBook-Air.local>
2015-03-25 08:05:43 -0400

    initial commit of treehouse profile app

---
+++ app/scripts/main.js
@@ -0,0 +1,121 @@
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function () {
+
+    if (xhr.readyState === 4) {
+        var data = JSON.parse(xhr.responseText);
+
+
+        //GET & SORT LIST OF SKILL POINTS EARNED
+        var pointsList = [];
+
+        // This is a list of colors to be associated with skills in Chart
+        var colorsObj = {
+            'Ruby' : '#E15258',
+            'iOS' : '#53BBB4',
+            'CSS' : '#3079AB',
+            'JavaScript' : '#C25975',
+            'Android' : '#5CB860',
+            'WordPress' : '#838CC7',
+            'Business' : '#F9845B',
+            'PHP' : '#7D669E',
+            'Design' : '#E59A13',
+            'HTML' : '#39ADD1',
+            'Python' : '#F092B0',
+            'Development Tools' : '#637A91',
+            'Java' : '#2C9676',
+            'Digital Literacy' : '#D4D9DD'
+        };
+
+        // Go through my points and add any skills I have
+        for (var key in data.points) {
+            if (data.points[key] > 0) {
+                pointsList.push([key, data.points[key], colorsObj[key]]);
+            }
+        }
+        // Sort array in order of highest to lowest
+        pointsList.sort(function (a, b) {
+            return b[1] - a[1];
+        });
+
+        // Sort the colors to be placed in chart
+        var chartColors = [];
+        for (var i = 1, j = pointsList.length; i < j; i++) {
+            chartColors.push(pointsList[i][2]);
+        }
+		
+		var pointsHTML = '<ul>';
+        // Generate the list items containing the skill points and values
+        for (var i = 1, j = pointsList.length; i < j; i++) {
+            var className = pointsList[i][0].replace(/\s+/g, '');
+            pointsHTML += '<li><span class="skill__color--' + className + '"></span>' + '<h3 class="skill__points">' + pointsList[i][1].toLocaleString() + '</h3>' + '<p class="skill__name">' + pointsList[i][0] + '</p></li>';
+        }
+
+        var profileHTML = '<h1>' + data.name + '</h1><span> : '+ pointsList[0][1].toLocaleString() + ' Total Points, '+ data.badges.length + ' Achievements</span>';
+        var badgeHTML = '<ul class="badges">';
+
+        for (var i = 0, j = data.badges.length - 1; i < j; j--) {
+            badgeHTML += '<li class="badge">';
+            badgeHTML += '<p class="badge--name">' + data.badges[j].name + '</p>';
+            badgeHTML += '<p class="badge--course">' + data.badges[j].courses[0].title + '</p>';
+            badgeHTML += '<img src="' + data.badges[j].icon_url + '">';
+            badgeHTML += '</li>';
+        }
+        // Add closing list tags for skill list and achievements
+        badgeHTML += '</ul>';
+        pointsHTML += '</ul>';
+
+
+        // document.getElementById('profile-image').innerHTML = '<img src="' + data.gravatar_url + '">';
+        document.getElementById('achievements').innerHTML = badgeHTML;
+        document.getElementById('skills__list').innerHTML = pointsHTML;
+        document.getElementById('profile').innerHTML = '<img src="' + data.gravatar_url + '">' + profileHTML;
+
+
+        // HIGH-CHARTS OPTIONS & CONSTRUCTER
+
+        Highcharts.setOptions({
+			lang: {
+				thousandsSep: ','
+			}
+		});
+        // Chart options
+        var options = {
+            chart: {
+                renderTo: 'skills__chart',
+                type: 'pie',
+		        spacingTop: 0,
+		        spacingRight: 0,
+		        spacingBottom: 0,
+		        spacingLeft: 0,
+		        marginTop: 0
+            },
+            title: {
+                text: ''
+            },
+          
+            series: [{
+                name: 'Points: ',
+                innerSize: '50%',
+                data: pointsList.slice(1)
+            }],
+            plotOptions: {
+                pie: {
+                    borderWidth: 0,
+                    colors: chartColors,
+                    dataLabels: {
+                    	enabled: false
+                    }
+                }
+            }
+        };
+        // Construct the chart with the given options
+        var chart = new Highcharts.Chart(options);
+    }
+
+};
+
+xhr.open('GET', 'http://teamtreehouse.com/patrickelhage.json');
+xhr.send();
+
+
