ffcd88e112ee549cafac67920d043bbcca0f7a8c
Patrick El-Hage <patrick.elhage@gmail.com>
2015-03-25 15:10:12 -0400

    Updated styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,10 +5,6 @@ xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
         var data = JSON.parse(xhr.responseText);
 
-
-        //GET & SORT LIST OF SKILL POINTS EARNED
-        var pointsList = [];
-
         // This is a list of colors to be associated with skills in Chart
         var colorsObj = {
             'Ruby' : '#E15258',
@@ -27,18 +23,23 @@ xhr.onreadystatechange = function () {
             'Digital Literacy' : '#D4D9DD'
         };
 
+        // This is an array containing each skill's name, point total, and color
+        var pointsList = [];
+
         // Go through my points and add any skills I have
         for (var key in data.points) {
             if (data.points[key] > 0) {
                 pointsList.push([key, data.points[key], colorsObj[key]]);
+                        // i.e. ['Javascript', 2430, '#C25975']
             }
         }
+
         // Sort array in order of highest to lowest
         pointsList.sort(function (a, b) {
             return b[1] - a[1];
         });
 
-        // Sort the colors to be placed in chart
+        // Sort the colors to be used in the Chart
         var chartColors = [];
         for (var i = 1, j = pointsList.length; i < j; i++) {
             chartColors.push(pointsList[i][2]);
@@ -51,29 +52,28 @@ xhr.onreadystatechange = function () {
             pointsHTML += '<li><span class="skill__color--' + className + '"></span>' + '<h3 class="skill__points">' + pointsList[i][1].toLocaleString() + '</h3>' + '<p class="skill__name">' + pointsList[i][0] + '</p></li>';
         }
 
-        var profileHTML = '<h1>' + data.name + '</h1><span> : '+ pointsList[0][1].toLocaleString() + ' Total Points, '+ data.badges.length + ' Achievements</span>';
-        var badgeHTML = '<ul class="badges">';
+        var profileHTML = '<h1><a href="'+data.profile_url+'">' + data.name + '</a></h1><span> : '+ pointsList[0][1].toLocaleString() + ' Total Points, '+ data.badges.length + ' Achievements</span>';
 
+        var badgeHTML = '<ul class="badges">';
         for (var i = 0, j = data.badges.length - 1; i < j; j--) {
             badgeHTML += '<li class="badge">';
-            badgeHTML += '<p class="badge--name">' + data.badges[j].name + '</p>';
+            badgeHTML += '<p class="badge--name"><a href="'+data.badges[j].url + '">' + data.badges[j].name + '</a></p>';
             badgeHTML += '<p class="badge--course">' + data.badges[j].courses[0].title + '</p>';
             badgeHTML += '<img src="' + data.badges[j].icon_url + '">';
             badgeHTML += '</li>';
         }
+
         // Add closing list tags for skill list and achievements
         badgeHTML += '</ul>';
         pointsHTML += '</ul>';
 
-
-        // document.getElementById('profile-image').innerHTML = '<img src="' + data.gravatar_url + '">';
+        // Add HTML to DOM
         document.getElementById('achievements').innerHTML = badgeHTML;
         document.getElementById('skills__list').innerHTML = pointsHTML;
         document.getElementById('profile').innerHTML = '<img src="' + data.gravatar_url + '">' + profileHTML;
 
 
         // HIGH-CHARTS OPTIONS & CONSTRUCTER
-
         Highcharts.setOptions({
 			lang: {
 				thousandsSep: ','
@@ -114,8 +114,12 @@ xhr.onreadystatechange = function () {
     }
 
 };
+var name = prompt('Hello, what is your name?');
+
+alert('Hi' + name + ', you are a FAT NERD');
 
 xhr.open('GET', 'http://teamtreehouse.com/patrickelhage.json');
 xhr.send();
+
 
 
